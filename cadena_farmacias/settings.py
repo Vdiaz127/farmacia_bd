82cbf2facb55f56f33535cd80e92ea66cd5e06b3
Vdiaz127 <arteagaj124@gmail.com>
2025-02-19 00:18:11 -0400

    UPDATE README.md

--- cadena_farmacias/settings.py
+++ cadena_farmacias/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "CADENA_FARMACIAS",
         "USER": "postgres",
-        "PASSWORD": "postgres",
+        "PASSWORD": "123456",
         "HOST": "127.0.0.1",
         "PORT": "5432",
     }
